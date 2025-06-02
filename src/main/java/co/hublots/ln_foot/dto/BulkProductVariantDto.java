1b9fb2f4f7647ecf30b035248dee7a8dd657815d
Marco Kuidja <kuidjamarco@gmail.com>
2025-06-02 16:47:03 +0100

    refactor: Add NoArgsConstructor to DTO classes for improved instantiation

--- src/main/java/co/hublots/ln_foot/dto/BulkProductVariantDto.java
+++ src/main/java/co/hublots/ln_foot/dto/BulkProductVariantDto.java
@@ -8,10 +8,12 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class BulkProductVariantDto {
     @Valid
     @Builder.Default
